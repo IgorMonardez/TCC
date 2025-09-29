2cb887d320c05308f98854c466f4d8599800df18
Igor de Paula Siqueira Monárdez <igormonardez@id.uff.br>
2025-09-29 12:46:16 -0300

    Esboço de algoritmo indutivo, com acurácia de 50%

--- algoritmo_indutivo.py
+++ algoritmo_indutivo.py
@@ -1,4 +1,5 @@
 import random
+from collections import Counter
 
 
 def vizinhanca(grafo, vertice):
@@ -44,7 +45,7 @@ def aplica_ativacao(grafo, ativacao):
     vertices_grafos = list(grafo.keys())
 
     for vertice in vertices_grafos:
-        grafo[vertice][1] = 1
+        grafo[vertice][1] = True
 
     for vertice in ativacao:
         if grafo[vertice][1]:
@@ -64,76 +65,73 @@ def aplica_ativacao(grafo, ativacao):
     return True
 
 
-def diferenca_simetrica(set_1, set_2):
-    return set_1 ^ set_2
-
-
 def algoritmo_indutivo(grafo):
     vertices = list(grafo.keys())
     n = len(vertices)
-
+    # print('n: ', n)
     if n == 0:
         return set()
-
-    if n == 1:
-        return set(list(grafo.keys())[0])
-
     v = random.choice(vertices)
+    # print('v: ', v)
+    if n == 1:
+        return {list(grafo.keys())[0]}
+
     grafo_v = retira_vertice(grafo, v)
 
     solucao_v = algoritmo_indutivo(grafo_v)
 
     if aplica_ativacao(grafo, solucao_v):
+        # print(f"aplicado: {solucao_v}")
         return solucao_v
 
     if n % 2 == 0:
-        vertices_v = vertices.copy()
-        vertices_v.remove(v)
-        paridade = {u: 0 for u in vertices}
-        for w in vertices_v:
+        # print("par")
+        solucao = []
+        for w in grafo_v:
+            # print('w: ', w)
             grafo_w = retira_vertice(grafo, w)
             solucao_w = algoritmo_indutivo(grafo_w)
-            for u in solucao_w:
-                paridade[u] = paridade[u] ^ 1
+            solucao = solucao_w ^ solucao_v
 
-        return {u for u in vertices if paridade[u] == 1}
+        return solucao
     else:
+        # print("impar")
         v = procura_vertice_par(grafo)
         grafo_v = retira_vizinhanca_fechada(grafo, v)
-        solucao = set()
+        solucao = {v}
         for w in grafo_v:
-            grafo_w = retira_vertice(grafo, w)
+            # print('w: ', w)
+            grafo_w = retira_vertice(grafo_v, w)
             solucao_w = algoritmo_indutivo(grafo_w)
-            solucao = diferenca_simetrica(solucao, solucao_w)
+            solucao = solucao.union(solucao_w)
 
-        result = {v}
-        result = diferenca_simetrica(solucao, result)
-        return result
+        return solucao
 
 
+#TODO: Alguns problemas, como a ordem da solução e de como fazer a união das soluções.
 if __name__ == '__main__':
     grafo = {
         '1': [['2', '3'], True],
         '2': [['1', '3', '4'], True],
-        '3': [['1', '2', '5'], True],
-        '4': [['2'], True],
-        '5': [['3'], True]
+        '3': [['1', '2'], True],
+        '4': [['2'], True]
     }
+
     contagem = 0
-    for i in range(100):
-        certo2 = {'1','2','3'}
-        certo3 = {'3','2','1'}
-        certo4 = {'2','3','1'}
-        certo5 = {'2','1','3'}
+    # grafo1 = {
+    #     '1': [['a', 'b'], True],
+    #     '2': [['a', 'b', 'c'], True],
+    #     '3': [['c'], True],
+    #     'a': [['1', '2'], True],
+    #     'b': [['1', '2'], True],
+    #     'c': [['3', '2'], True],
+    # }
+    n = 1000
+    for i in range(n):
         result = algoritmo_indutivo(grafo)
-        if result == certo2:
+        if aplica_ativacao(grafo, result):
             contagem += 1
-        elif result == certo3:
-            contagem += 1
-        elif result == certo4:
-            contagem += 1
-        elif result == certo5:
-            contagem += 1
+            print("correto: ", result)
         else:
-            print(result)
-    print(contagem/100)
+            print("errado: ", result)
+    print(contagem / n)

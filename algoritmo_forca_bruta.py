5403b37b0868c93e5872fbae2b833208c54c1652
Igor de Paula Siqueira Monárdez <igormonardez@id.uff.br>
2025-10-14 14:18:14 -0300

    Fazendo o algoritmo de força bruta e atualização dos metodos para calcular a optimalidade.

---
+++ algoritmo_forca_bruta.py
@@ -0,0 +1,85 @@
+import random
+
+
+def ativa_grafo(grafo):
+    for v in grafo.keys():
+        grafo[v][1] = True
+
+def verifica_ativacao(ativacao, grafo):
+    vertices_ativacao = ativacao.keys()
+
+    ativa_grafo(grafo)
+
+    for v in vertices_ativacao:
+        if grafo[v][1]:
+            grafo[v][1] = False
+        else:
+            grafo[v][1] = True
+        for vizinho in grafo[v][0]:
+            if grafo[vizinho][1]:
+                grafo[vizinho][1] = False
+            else:
+                grafo[vizinho][1] = True
+
+    for v in grafo.keys():
+        if grafo[v][1]:
+            return False
+
+    return True
+
+
+def gera_subgrafos(grafo):
+    vertices = list(grafo.keys())
+    random.shuffle(vertices)
+    n = len(vertices)
+    min_subgraph = 9999999999999
+    all_subgraphs = []
+    for mask in range(1, 1 << n):
+        subgraph_vertices = []
+        vertex_index_map = {}
+
+        for i in range(n):
+            if mask & (1 << i):
+                vertex = vertices[i]
+                subgraph_vertices.append(vertex)
+                vertex_index_map[vertex] = len(subgraph_vertices) - 1
+
+        subgraph = {}
+        for vertex in subgraph_vertices:
+            neighbors = []
+            for neighbor in grafo[vertex][0]:
+                if neighbor in vertex_index_map:
+                    neighbors.append(neighbor)
+            subgraph[vertex] = neighbors
+        all_subgraphs.append(subgraph)
+
+    return all_subgraphs
+
+def algoritmo_forca_bruta(grafo):
+    subgrafos = gera_subgrafos(grafo)
+
+    ativacao = []
+    tamanho_ativacao = 99999999999999
+
+    for subgrafo in subgrafos:
+        if verifica_ativacao(subgrafo, grafo):
+            if tamanho_ativacao > len(subgrafo):
+                ativacao = subgrafo
+                tamanho_ativacao = len(subgrafo)
+
+    return ativacao, tamanho_ativacao
+
+if __name__ == '__main__':
+    grafo_simples = {
+        '1': [['2', '6'], True],
+        '2': [['1', '3'], True],
+        '3': [['2', '4'], True],
+        '4': [['3', '5'], True],
+        '5': [['4', '6'], True],
+        '6': [['5', '1'], True]
+    }
+
+    ativacao, tamanho_ativacao = algoritmo_forca_bruta(grafo_simples)
+
+    print(ativacao)
+    print(tamanho_ativacao)
--- metodos.py
+++ metodos.py
@@ -1,3 +1,8 @@
+import copy
+
+from algoritmo_forca_bruta import algoritmo_forca_bruta
+from algoritmo_guloso import algoritmo_guloso
+from algoritmo_indutivo import algoritmo_indutivo,aplica_ativacao
 
 def gera_grafo_igraph(grafo_igraf):
     grafo_dict = {}
@@ -8,3 +13,54 @@ def gera_grafo_igraph(grafo_igraf):
         grafo_dict[str(vertice)] = [vizinhos, True]
 
     return grafo_dict
+
+def printa_resultados(grafo):
+    n = 10
+    contagem = 0
+    grafo = copy.deepcopy(grafo)
+    print(grafo)
+
+    otimo, tamanho_otimo = algoritmo_forca_bruta(grafo)
+    # print("guloso: ", resultado_guloso(grafo, n))
+    print("indutivo: ", resultado_indutivo(grafo, n, tamanho_otimo))
+
+def calcula_optimalidade(tamanho_otimo, ativacao):
+    if tamanho_otimo == len(ativacao):
+        return True
+    return False
+
+def resultado_guloso(grafo, n, tam_otimo):
+    contagem_corretude = 0
+    contagem_optimidade = 0
+    for i in range(n):
+        result = algoritmo_guloso(grafo)
+        if aplica_ativacao(grafo, result):
+            contagem_corretude += 1
+            if calcula_optimalidade(tam_otimo, result):
+                contagem_optimidade += 1
+                print("otimo: ", result)
+            print("apenas correto: ", result)
+        else:
+            print("errado: ", result)
+
+    optimidade = contagem_optimidade / n
+    corretude = contagem_corretude / n
+    return optimidade, corretude
+
+def resultado_indutivo(grafo, n, tam_otimo):
+    contagem_corretude = 0
+    contagem_optimidade = 0
+    for i in range(n):
+        result = algoritmo_indutivo(grafo)
+        if aplica_ativacao(grafo, result):
+            contagem_corretude += 1
+            if calcula_optimalidade(tam_otimo, result):
+                contagem_optimidade += 1
+                print("otimo: ", result)
+            print("apenas correto: ", result)
+        else:
+            print("errado: ", result)
+
+    optimidade = contagem_optimidade / n
+    corretude = contagem_corretude / n
+    return optimidade, corretude
